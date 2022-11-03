95cebde273657be3fc59413aa1d039fb6355302f
Legolitas12 <samuel.campeon@gmail.com>
2022-11-03 14:11:04 -0500

    Homework 06 completa

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,35 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    return numeros.reduce((prev, current) => prev + current, 0);
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  return array.filter((e) => e % 2 === 0);
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map((num) => num ** 2);
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce((prev, next) => prev + next, array[0]);
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+
 }
   
   
